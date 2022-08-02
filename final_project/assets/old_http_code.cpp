908a8b044a36ce0912254a652a5bc6431ba81e18
alan-tan90 <akhoultchaev@branfordschools.org>
2022-08-02 03:27:55 -0400

    Updated: Final Project

---
+++ final_project/assets/old_http_code.cpp
@@ -0,0 +1,64 @@
+
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClient client = server.available();
+    if (client) {
+      String currentLine = "";
+      while (client.connected()) {
+        Serial.println("Client Connected.");
+        if (client.available()) {
+          char c = client.read();
+          Serial.write(c);
+          header += c;
+          if (c == '\n') {                    
+            if (currentLine.length() == 0) {
+              // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+              // and a content-type so the client knows what's coming, then a blank line:
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-type:text/html");
+              client.println("Connection: close");
+              client.println();
+              client.println("<!DOCTYPE html><html lang=\"en\">");
+              client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+              client.println("<link rel=\"icon\" href=\"data:,\">");
+              // CSS to style the on/off buttons
+              // Feel free to change the background-color and font-size attributes to fit your preferences
+              client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+              client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+              client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+              client.println(".button2 {background-color: #555555;}</style></head>");
+
+              // Web Page Heading
+              client.println("<body><h1>ESP32 Web Server</h1>");
+
+              
+
+              // The HTTP response ends with another blank line
+              client.println();
+              // Break out of the while loop
+              break;
+            } else { // if you got a newline, then clear currentLine
+              currentLine = "";
+            }
+          } else if (c != '\r') {  // if you got anything else but a carriage return character,
+            currentLine += c;      // add it to the end of the currentLine
+          }
+        }
+      }
+      // Clear the header variable
+      header = "";
+      // Close the connection
+      client.stop();
+      Serial.println("Client disconnected.");
+      Serial.println("");
+      delay(1000);
+    }
+  } else {
+    Serial.println("An error occurred!");
+  }
+  /*static HTTPClient http;
+    http.begin(website);
+    int response = http.GET();
+    if(response > 0) {
+    String payload = http.getString();
+    }
+    http.end(); */
